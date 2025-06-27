ebdd593e1220be5164944949b93b8165c0279871
Christoph Zust <christoph.zust@statistik.ji.zh.ch>
2025-06-27 14:30:03 +0200

    fix geo-testing

--- tests/testthat/test-create_geo_output.R
+++ tests/testthat/test-create_geo_output.R
@@ -5,24 +5,50 @@ testthat::test_that("spatial information is added correctly", {
 
   # xml to df to sf
   xml_file <- testthat::test_path("fixtures/geo_test.xml")
-  sf_for_map <- create_baupub_df(xml_file) |>
+  sf_map <- create_baupub_df(xml_file) |>
     add_spatial_information(sf_liegenschaften = sf_liegenschaften_test)
-
-  # check if sf was correctly built
-  sf_for_map_test <- sf::st_read(
+  
+  
+  # unnest multipolygon as the github-instances seems to load sub-polygons in a
+  # random order
+  sf_map_cast <- suppressWarnings(sf::st_cast(sf_map, "POLYGON"))
+  
+  first_polygon_entry <- sapply(1:3, function(x) sf_map_cast$geom[[x]][[1]][[1]])
+  
+  sf_map_cast <- sf_map_cast |> 
+    dplyr::mutate(order = first_polygon_entry) |> 
+    dplyr::arrange(order)
+  
+  
+  
+  # prepare data that should be tested against
+  sf_test <- sf::st_read(
     dsn = dsn, layer = "test_sf_map",
     crs = 2056
   )
-
-  sf_for_map <- sf::st_set_precision(sf_for_map, 1e-6)
-  sf_for_map_test <- sf::st_set_precision(sf_for_map_test, 1e-6)
   
-  # Ensure CRS matches
-  sf::st_crs(sf_for_map) <- sf::st_crs(sf_for_map_test)
-
-  coordinate_equality_test <- all(sf::st_equals_exact(sf_for_map, sf_for_map_test, par = 1e-6, sparse = FALSE))
+  sf_test_cast <- suppressWarnings(sf::st_cast(sf_test, "POLYGON"))
   
-  testthat::expect_true(coordinate_equality_test)
+  first_polygon_entry_test <- sapply(1:3, function(x) sf_test_cast$geom[[x]][[1]][[1]])
+  
+  sf_test_cast <- sf_test_cast |> 
+    dplyr::mutate(order = first_polygon_entry_test) |> 
+    dplyr::arrange(order)
+  
+  
+  
+  
+  # check if sf was correctly built
+  coordinate_equality_test <- sapply(1:3, function(x) {
+    
+    
+    all(sf::st_equals_exact(sf_map_cast[x,], 
+                            sf_test_cast[x,], 
+                            par = 1e-6, 
+                            sparse = FALSE))
+  })
+  
+  testthat::expect_true(all(coordinate_equality_test))
   
 })
 
@@ -33,12 +59,12 @@ testthat::test_that("a map is create", {
   sf_liegenschaften_test <- sf::st_read(dsn = dsn, layer = "test_liegenschaften")
 
   # check if sf was correctly built
-  sf_for_map_test <- sf::st_read(
+  sf_map_test <- sf::st_read(
     dsn = dsn, layer = "test_sf_map",
     crs = 2056
   )
 
-  map <- sf_for_map_test |> create_map()
+  map <- sf_map_test |> create_map()
   
 
   testthat::expect_equal(inherits(map, "leaflet"), TRUE)
